30b672fc9876fdc96d246b2ee57f3e364dc57726
Robert Kroeger <rjkroege@liqui.org>
2024-03-11 08:36:18 -0400

    Parse the output
    Use the participle parser to (hopefully) robustly parse the
    output of the AppleScript. Participle seems to work well.

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/rjkroege/tabsearch
 
 go 1.22.0
+
+require (
+	github.com/alecthomas/participle/v2 v2.1.1 // indirect
+	github.com/sanity-io/litter v1.5.5 // indirect
+)
--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"os/exec"
 	"os"
+
+	"github.com/alecthomas/participle/v2"
+	"github.com/sanity-io/litter"
 )
 
 const scriptsource = `tell application "Safari"
@@ -19,6 +22,17 @@ end tell
 
 const command = "/usr/bin/osascript"
 
+type Value struct {
+	String *string  `@String`
+}
+
+type Tabs struct  {
+	Props []*PropArray `"{" @@  "," @@ "}"`
+}
+
+type  PropArray struct {
+	Values []*Value `"{" ( @@ ( "," @@ )* )? "}"`
+}
 
 func main() {
 	log.Println("goody")
@@ -37,6 +51,22 @@ func main() {
 	// It's not valid JSON. I need a parser. A simple parser.
 	log.Println("output", string(output))
 
-
+	parser, err := participle.Build[Tabs]()
+	if err != nil {
+		log.Println("parser creation failed", err)
+		os.Exit(-2)
+	}
 	
+	ast, err := parser.ParseBytes("", output)
+	if err != nil {
+		log.Println("parsing failed", err)
+		os.Exit(-3)
+	}
+
+	// Example: how to dump the AST
+	log.Println(*ast.Props[0].Values[0].String)
+
+	litter.Dump(ast)
+
+	// TODO(rjk): 
 }
